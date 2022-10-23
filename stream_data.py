c95ac6afefa13975c147ed35d7ebb76006929d68
Hellman Oded <oded.hellman@gmail.com>
2022-10-23 17:57:49 +0300

    Added sub and save methods

---
+++ stream_data.py
@@ -0,0 +1,22 @@
+from os import getenv
+import csv
+from redis.client import Redis
+from schema.retail_schema import *
+
+MAX_ROWS_TO_STREAM = 1000
+
+password = getenv('REDIS_PASSWORD')
+host = getenv('REDIS_HOST')
+port = getenv('REDIS_PORT')
+
+r = Redis(password=password, host=host, port=port, decode_responses=True)
+
+with open ('data/online_retail.csv', encoding='utf-8') as input_file:
+    dict_reader = csv.DictReader(input_file)
+    
+    for row_num, line in enumerate(dict_reader):
+        if row_num == MAX_ROWS_TO_STREAM:
+            break
+        print(f'Order num: {row_num} ---', line)
+        r.xadd(name='Orders', fields=line)
+        
---
+++ sub_and_save.py
@@ -0,0 +1,44 @@
+from os import getenv
+from redis import Redis
+import datetime
+import time
+from pydantic import ValidationError
+from schema.retail_schema import  *
+
+password = getenv('REDIS_PASSWORD')
+host = getenv('REDIS_HOST')
+port = getenv('REDIS_PORT')
+
+r = Redis(password=password, host=host, port=port, decode_responses=True)
+
+while True:
+	received = r.xread(streams={"Orders": '$'}, count=100, block=0)
+
+	for result in received:
+		data = result[1]
+		for tuple in data:
+			orderDict = tuple[1]
+			print(orderDict)
+
+			try:
+				item = Product(
+					StockCode=orderDict['StockCode'],
+					Description=orderDict['Description'],
+					UnitPrice=orderDict['UnitPrice']
+				)
+
+				order = Order(
+					InvoiceNo=orderDict['InvoiceNo'],
+					Item = item,
+					Quantity=orderDict['Quantity'],
+					InvoiceDate=datetime.datetime.strptime(orderDict['InvoiceDate'], '%m/%d/%Y %H:%M'),
+					CustomerID=orderDict['CustomerID'],
+					Country=orderDict['Country']
+				)
+            
+			except (ValidationError, KeyError) as e:
+				print(e)
+				continue
+
+			print(order.key())
+			order.save()
